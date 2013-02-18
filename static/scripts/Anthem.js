b2c43e515dbcf655d811b8bf35e86890ca915c21
Michael Caron <michael.r.caron@gmail.com>
2013-02-17 22:32:02 -0600

    Added static files

---
+++ static/scripts/Anthem.js
@@ -0,0 +1,9 @@
+var questions = [
+//    {cat:"National Anthem",questions:[
+        {q:"Give another name for 'The Star-Spangled Banner'."},
+        {q:"Who wrote the Star-Spangled Banner?"},
+        {q:"What was the Star-Spangled Banner before it was a song?"},
+        {q:"On what kind of vehicle was the Star-Spangled Banner written?", c:["ship","motorcycle","covered wagon","piggy-back ride"]},
+        {q:"According to the song, what does the Star-Spangled Banner wave over?"}
+//    ]},
+];
---
+++ static/scripts/questions.js
@@ -0,0 +1,70 @@
+var questions = [ //{
+//    "Anthem":[
+        {q:"Give another name for 'The Star-Spangled Banner'."},
+        {q:"Who wrote the Star-Spangled Banner?"},
+        {q:"What was the Star-Spangled Banner before it was a song?"},
+        {q:"On what kind of vehicle was the Star-Spangled Banner written?", c:["ship","motorcycle","covered wagon","piggy-back ride"]},
+        {q:"According to the song, what does the Star-Spangled Banner wave over?"},
+//    ],
+//    "People":[
+        {q:"What is the name of the first President of the United States of America?"},
+        {q:"Who is our current President?"},
+        {q:"Who is our current vice president?"},
+        {q:"Who is our current governor?"},
+        {q:"Who is the chief executive officer of Sun Prairie? (hint: we met him at City Hall)"},
+//    ],
+//    "FlagEttiquette":[
+        {q:"What can the flag not be allowed to touch?", c:["a monkey foot","the wind","the ground","dirty hands"]},
+        {q:"Only when in this can you salute the flag:", c:["a car","your class A uniform", "your class B uniform", "your pajamas"]},
+        {q:"What shape does a properly folded flag make?"},
+        {q:"How is the flag flown in times of distress?", c:["upside down", "sideways", "with the colors switched","with flashing lights"]},
+//    ],
+//    "Symbols":[
+        {q:"What's the name of the President's home?"},
+        {q:"According to the Declaration of Independance, all people were created in what way?", c:["human","nice","having a set of eyes","equal"]},
+        {q:"What is the Statue of Liberty holding in her left hand?",c:["a burger","a flashlight", "a torch", "a tablet"]},
+        {q:"What is the presidential airplane called?"},
+        {q:"What does Uncle Sam wear on his head?"},
+//    ],
+//    "Pledge":[
+        {q:"Where do we place our hand when we say the Pledge of Allegiance?"},
+        {q:"What does the word 'PLEDGE' mean?"},
+        {q:"What does the word 'INDIVISIBLE' mean?"},
+        {q:"What two words were added to the pledge in the 1950s?"},
+        {q:"What does the word 'ALLEGIANCE' mean?"},
+//    ],
+//    "FlagHistory":[
+        {q:"Who is said to have sewn the first flag?"},
+        {q:"What symbol on the flag represents the 50 states?"},
+        {q:"When is Flag Day?"},
+        {q:"What flag was used by most colonies before the American one?"},
+//    ],
+//    "FlagRespect":[
+        {q:"What do you do with your hands while the flag is raised or lowered?"},
+        {q:"Where do you place your hand when NOT in uniform during the pledge?"},
+        {q:"What day in September should the flag NOT fly?"},
+        {q:"How do you dispose of an old, wornout flag?"},
+//    ],
+//    "StateNames":[
+        {q:"What stat name has 'island' in it?"},
+        {q:"What state name has an indian?"},
+        {q:"What state name is cut on the end?"},
+        {q:"What state name has a ton?"},
+        {q:"What state name has a tuck in the middle?"},
+//    ],
+//    "YourRights":[
+        {q:"What right allows you to pray as you like?"},
+        {q:"What right allows you to say what you think?"},
+        {q:"What right allows you to choose members of government?"},
+        {q:"What right allows you to create a group of people to discuss problems?"},
+        {q:"What paper is required before the police can search your home?"},
+//    ],
+//    "OddsEnds":[
+        {q:"What activity badge are we working on?"},
+        {q:"What is the capitol of Wisconsin?"},
+        {q:"What capitol is NOT in a state?"},
+        {q:"What day in January should the flag not fly?"},
+        {q:"What do you say to promise loyalty to your country?"},
+//    ],
+//};
+];
