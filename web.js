291237180dac6dfe292a92601c40583fbbd0d641
Michael Caron <michael.r.caron@gmail.com>
2013-02-17 22:31:39 -0600

    created express structure for serving files

--- web.js
+++ web.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express.createServer(express.logger());
 
-app.get('/', function(req,res) {
-	response.send('Hello World!');
+app.configure(function() {
+	app.use(express.static(__dirname + '/static'));
 });
 
 var port = process.env.PORT || 5000;
